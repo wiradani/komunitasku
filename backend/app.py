d8534b226ed57b3121325c8db1972a349d9a9880
Wiradani Ramadhan <adhan432@gmail.com>
2018-05-05 16:33:36 +0700

    crud user+reward

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask,request,jsonify
 from user import User
+from rewards import  Rewards
 
 app = Flask(__name__)
 user = User()
+reward = Rewards()
 
 @app.route("/")
 def hello():
@@ -63,6 +65,54 @@ def updateUser():
     else:
         return "must be POST method"
 
+@app.route('/createReward',methods=['POST','GET'])
+def createReward():
+    if request.method == "POST":
+        nama_reward = request.form["nama_reward"]
+        deskripsi_reward = request.form["deskripsi_reward"]
+        syarat_point = request.form["syarat_point"]
+        res = reward.createReward(nama_reward,deskripsi_reward,syarat_point)
+        message = {
+            'status': 200,
+            'message': 'success',
+            'data' : [nama_reward,deskripsi_reward,syarat_point]
+        }
+        return jsonify(message)
+    else:
+        return reward.createReward("test","test","100")
+
+@app.route('/deleteReward',methods=['POST','GET'])
+def deleteReward():
+    if request.method == "POST":
+        id = request.form["id_reward"]
+        res = reward.deleteReward(id)
+        message = {
+            'status': 200,
+            'message': 'success',
+        }
+        return jsonify(message)
+    else:
+        return "must be POST method"
+
+@app.route('/updateReward',methods=['POST','GET'])
+def updateReward():
+    if request.method == "POST":
+        id_reward = request.form["id_reward"]
+        nama_reward = request.form["nama_reward"]
+        deskripsi_reward = request.form["deskripsi_reward"]
+        syarat_point = request.form["syarat_point"]
+        res = reward.updateReward(id_reward,nama_reward,deskripsi_reward,syarat_point)
+        message = {
+            'status': 200,
+            'message': 'success',
+            'data' : [id_reward,nama_reward,deskripsi_reward,syarat_point]
+        }
+        if res:
+            return jsonify(message)
+        else:
+            return "NO"
+    else:
+        return "must be POST method"
 
 
 if __name__ == '__main__':
--- backend/rewards.py
+++ backend/rewards.py
@@ -7,10 +7,10 @@ class Rewards:
         self.db = MySQLdb.connect(user="root",passwd="",db="KomunitasKu",unix_socket="/opt/lampp/var/mysql/mysql.sock")
         self.cursor = self.db.cursor()
 
-    def membuatReward(self,nama_reward,deskripsi_reward,syarat_point):
+    def createReward(self,nama_reward,deskripsi_reward,syarat_point):
         list_arg = [nama_reward,deskripsi_reward,syarat_point]
         val = '","'.join(list_arg)
-        sql='insert into reward(nama_reward,deskripsi_reward,syarat_point,email) values ("'+val+'")'
+        sql='insert into reward(nama_reward,deskripsi_reward,syarat_point) values ("'+val+'")'
         try:
             self.cursor.execute(sql)
             self.db.commit()
@@ -19,3 +19,35 @@ class Rewards:
             print("error")
             self.db.rollback()
         return "ok data berhasil ditambah"
+
+    def deleteReward(self,id_reward):
+        sql='DELETE from reward where id_reward='+str(id_reward)+';'
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+            print("berhasil")
+        except:
+            print("error")
+            self.db.rollback()
+        return  "data berhasil dihapus"
+
+    def updateReward(self,id_reward,nama_reward,deskripsi_reward,syarat_point):
+        list_arg=[nama_reward,deskripsi_reward,syarat_point]
+        val_arg=[]
+        if list_arg[0]!="":
+            val_arg.append("nama_reward='"+nama_reward+"'")
+        if list_arg[1]!="":
+            val_arg.append("deskripsi_reward='"+deskripsi_reward+"'")
+        if list_arg[2]!="":
+            val_arg.append("syarat_point='"+syarat_point+"'")
+
+        val=','.join(val_arg)
+        sql='update reward set '+val+' where id_reward='+str(id_reward)+';'
+
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+            return True
+        except:
+            self.db.rollback()
+            return False
